a98b768b0de7ad996acbb28e8cec23711fd1f8d6
Muhammad Talha Akbar <talha.akbar@ebryx.com>
2020-01-20 01:46:02 +0500

    Fixed submission is undefined error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+@if(isset($submission))
 <script>
     var submissionForm = <?php echo json_encode($submission) ?>;
 </script>
+@endif
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
